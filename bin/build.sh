d7c9f5869dd69b54fbdd0f9ceb6c5dee3a6a2054
Jihyung Song <bluese@megazone.com>
2020-12-28 11:44:17 +0900

    remove bin directory (not used)

--- bin/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Build a docker image
-docker build -t pyengine/aws-power-state . --no-cache
-
-docker tag pyengine/aws-power-state pyengine/aws-power-state:1.1
-docker tag pyengine/aws-power-state spaceone/aws-power-state:1.1
